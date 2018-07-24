69d0018d5494d31c90d55f2008d55e925167cb7f
Anton Sokolovskii <a.sokolovsky15@gmail.com>
2018-07-24 15:08:54 +0300

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,6 +2,6 @@
 # https://github.com/mmulqueen/pyStrich
 from pystrich.datamatrix import DataMatrixEncoder
 
-encoder = DataMatrixEncoder('This is a DataMatrix.')
+encoder = DataMatrixEncoder('This is a DataMatrix.'
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
